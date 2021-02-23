7546e4f1aa38bc99f7dd8144510036b5e5cb0502
ashwini <ashrajag@iu.edu>
2021-02-22 22:48:00 -0700

    removing logo testing build

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
         <p>
           Hi! I am Ashwini Rajagopal.
         </p>
-        
-      </header>
-    </div>
+        </div>
   );
 }
 
